4f82d917cfd706114934f65346003707dac6faf3
AgustinaComas <you@example.com>
2022-11-05 11:51:48 -0300

    eventos

--- src/app/inicio/eventos.service.ts
+++ src/app/inicio/eventos.service.ts
@@ -23,7 +23,20 @@ export class EventosService {
       Descripcion: 'Distruta de esta pelicula en familia en el cine',
       Imagen: "minions.img"
      },
-     
+     { 
+      id: '6',
+      Nombre: "Concierto Abel Pintos", 
+      Descripcion: 'El artista, uno de los más importantes de la escena argentina del último tiempo, llegará hoy al Autódromo rosarino con su gira "Abel en Concierto", en un recital que se destacará en la ciudad por medir y reducir su impacto a través de acciones ambientales.',
+      Imagen: "Abel.img"
+     },
+     { 
+      id: '7',
+      Nombre: "Start up weekend", 
+      Descripcion: 'Startup Week Rosario es una semana de conversatorios, workshops, talleres y espacios de networking. Del 31/10 al 3/11 animate a convertirte en experto en Economía circular y colaborativa, Emprender, Construir comunidad y Las tecnologías que se vienen',
+      Imagen: "Startup.img"
+     },
+
+
   ]
 
   constructor() { }
